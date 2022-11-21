86d94aa0d21aecd89871a3861276d308e7378fa1
Wikoss95 <nowak.wiktor95@gmail.com>
2022-11-21 06:56:39 +0100

    New commit

---
+++ webapp/routes.py
@@ -0,0 +1,14 @@
+from webapp import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/addexpense')
+def addexpense():
+    return render_template('addexpense.html')
+
